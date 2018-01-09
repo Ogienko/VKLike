71e47afbba27d952dd8c72acdf3585d3afe0063a
Evgeniy Ogienko <mister.ogienko@gmail.com>
2018-01-09 17:53:06 +0300

    Added Ajax request

---
+++ VKLike/Scripts/likes.js
@@ -0,0 +1,13 @@
+﻿function getDateTime() {
+    var date = new Date();
+    var result = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds() + "." + date.getMilliseconds();
+    return result;
+}
+
+function addLike(page) {
+    var data = {
+        DateTime: getDateTime(),
+        Page: page
+    }
+    $.post("/api/likes/add/", data);
+}
