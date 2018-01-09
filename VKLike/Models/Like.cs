b8e9bc3c28e5d6703374716652580348beb5ca6c
Evgeniy Ogienko <mister.ogienko@gmail.com>
2018-01-09 16:52:08 +0300

    Added context

---
+++ VKLike/Models/Like.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace VKLike.Models {
+
+    public class Like {
+
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Page { get; set; }
+    }
+}
---
+++ VKLike/Models/VKLikeContext.cs
@@ -0,0 +1,11 @@
+﻿using System.Data.Entity;
+
+namespace VKLike.Models {
+
+    public class VKLikeContext : DbContext {
+
+        public VKLikeContext() : base("VKLikeConnection") { }
+
+        public DbSet<Like> Likes { get; set; }
+    }
+}
